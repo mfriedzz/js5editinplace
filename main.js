1e7ca9c8c0ac61a5f4b665539de5ef0c860b3ddf
Michael F <mfriedmanzz01@gmail.com>
2015-01-22 18:07:01 -0700

    more

--- main.js
+++ main.js
@@ -4,18 +4,20 @@ $(document).on('ready', function() {
   		var position = $(this).width();
 		console.log("the width of the var position is:", position);
   		
-  		var text = $(this).after("<input class='newinput' type ='text' > ");
+  	 $(this).after("<input class='newinput' type ='text' > ");
+  		
   		$(this).hide(1000);
   		$('.newinput').width(position);
+  		
   	
   	// blur event after text is input
 
   		$('.newinput').change (function() {
-  			
-  			$(this).blur('text');
-  		//	$('.newinput').hide();
-  			$(this).text(text);
-  			console.log("I got here" ,text);
+  		var text1 = $('.newinput').val();
+  		$('.newinput').blur();
+  		$('.newinput').hide();
+  		$('p').append('text1');
+  			console.log("I got here", text1 );
   		});
 
   		
