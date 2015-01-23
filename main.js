f2afe696432cdda8ddfbf11f8e91bc74743ad465
Michael F <mfriedmanzz01@gmail.com>
2015-01-23 12:25:46 -0700

    more

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
 $(document).on('ready', function() {
   
   $( 'p' ).click(function() {
+  		
+  		var original = $(this);
+  		console.log(original);
   		var position = $(this).width();
 		console.log("the width of the var position is:", position);
   		
-  	 $(this).after("<input class='newinput' type ='text' > ");
+  	 	$(this).after("<input class='newinput' type ='text' > ");
   		
   		$(this).hide(1000);
   		$('.newinput').width(position);
+
+  		
   		
   	
   	// blur event after text is input
 
   		$('.newinput').change (function() {
-  		var text1 = $('.newinput').val();
-  		$('.newinput').blur();
-  		$('.newinput').hide();
-  		$('p').append('text1');
-  			console.log("I got here", text1 );
+  				var text2 = $('.newinput').val();
+  				$('.newinput').blur();
+  				$('.newinput').hide();
+  			
+  			//original(an object) has the initial paragraph clicked, and then original is passed the 
+  			//input text, and then the text method is available to the original object. 	
+  				original.text(text2);
+  				$(original).show();
+
+  			//	$('.newinput').append(
+  				// $('p').append('text1');
+  				console.log("I got here", text2 );
   		});
 
   		
-});
+	});
 
   
 });
